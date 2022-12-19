6a0282ef4489f9e556a22b54ad5ebf70be53b27e
Jess <Owner@USER>
2022-12-18 20:50:17 -0500

    dfdsfdsgdv

--- AimLab/Assets/Scripts/PlayerShooter.cs
+++ AimLab/Assets/Scripts/PlayerShooter.cs
@@ -11,7 +11,7 @@ public class PlayerShooter : MonoBehaviour
     private float smoothing;
 
     private GameObject player;
-
+    public bool hit;
     // Start is called before the first frame update
     void Start()
     {
--- AimLab/Assets/Scripts/Target.cs
+++ AimLab/Assets/Scripts/Target.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    public Vector3 position;
+    public Vector3 position, size;
     // Start is called before the first frame update
     void Start()
     {
         position.x = gameObject.transform.position.x;
         position.y = gameObject.transform.position.y;
         position.z = gameObject.transform.position.z;
+        size = GetComponent<Renderer>().bounds.size;
     }
 
     // Update is called once per frame
